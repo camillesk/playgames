ab74d491528700fe88f4ae1aab8a970a8e8296e4
CyanMarine <58150571+PrincessCyanMarine@users.noreply.github.com>
2022-06-22 21:27:52 -0300

    Verificação de CPF e bloqueio de submit caso invalido

--- js/client_register.js
+++ js/client_register.js
@@ -1,37 +1,45 @@
+const forEachOfClass = (className, func) =>
+  Array.from(document.getElementsByClassName(className)).forEach(func);
+const addClass = (element, className) =>
+  !element.className.includes(className)
+    ? (element.className += ` ${className}`)
+    : void 0;
+const removeClass = (element, className) =>
+  (element.className = element.className.replace(
+    new RegExp(`\\s*${className}`, "g"),
+    ""
+  ));
+
+const testEmpty = (element) =>
+  element.value.length == 0
+    ? removeClass(element, "not_empty")
+    : addClass(element, "not_empty");
+
 function onLoad() {
-  document
-    .getElementById("zip-code")
-    .addEventListener("input", onZipCodeChange);
+  const zipcode = document.getElementById("zip-code");
+  zipcode.addEventListener("input", onZipCodeChange);
+  if (zipcode.value) onZipCodeChange();
+
+  document.getElementById("cpf").addEventListener("input", onCPFChange);
 }
 
-/**
- *
- * @param {Event} ev
- */
-function onZipCodeChange(ev) {
-  const { target } = ev;
+function onZipCodeChange() {
+  const target = document.getElementById("zip-code");
+  testEmpty(target);
   const { value } = target;
-  const zipErrorElement = document.getElementById("zip-code-error");
-  setZipCodeResponse();
-  zipErrorElement.setAttribute("shown", false);
+  handleZipCodeResponse();
+
   if (value.length < 8) return;
-  const fetchURL = `https://viacep.com.br/ws/${value}/json`;
-  fetch(fetchURL)
+  fetch(`https://viacep.com.br/ws/${value}/json`)
     .then((res) => res.json())
-    .then((data) => {
-      setZipCodeResponse(data);
-    })
+    .then(handleZipCodeResponse)
     .catch((...err) => {
       console.error(...err);
-      zipErrorElement.setAttribute("shown", true);
-
-      forEachOfClass("zip-code-dependent", (element) =>
-        addClass(element, "hidden")
-      );
+      setCPFInvalido();
     });
 }
 
-function setZipCodeResponse(
+function handleZipCodeResponse(
   { bairro, localidade, logradouro, uf, erro } = {
     erro: "true",
   }
@@ -40,36 +48,72 @@ function setZipCodeResponse(
   const zipCodeElement = document.getElementById("zip-code");
 
   if (erro) {
-    document.getElementById("zip-code-error").setAttribute("shown", "true");
     bairro = "";
     localidade = "";
     logradouro = "";
     uf = "";
-
-    forEachOfClass("zip-code-dependent", (element) =>
-      addClass(element, "hidden")
-    );
+    setCPFInvalido();
   } else {
     zipCodeElement.blur();
-    forEachOfClass("zip-code-dependent", (element) =>
-      removeClass(element, "hidden")
-    );
+    setCPFValido();
   }
+
   document.getElementById("street").value = logradouro;
   document.getElementById("neighborhood").value = bairro;
   document.getElementById("city").value = localidade;
   document.getElementById("state").value = uf.toLowerCase();
 }
 
-const addClass = (element, className) =>
-  !element.className.includes(className)
-    ? (element.className += className)
-    : void 0;
-const removeClass = (element, className) =>
-  (element.className = element.className.replace(
-    new RegExp(className, "g"),
-    ""
-  ));
+function setCPFInvalido() {
+  document.getElementById("zip-code").setCustomValidity("CEP invalido");
+  forEachOfClass("zip-code-dependent", (element) =>
+    addClass(element, "hidden")
+  );
+}
 
-const forEachOfClass = (className, func) =>
-  Array.from(document.getElementsByClassName(className)).forEach(func);
+function setCPFValido() {
+  document.getElementById("zip-code").setCustomValidity("");
+  forEachOfClass("zip-code-dependent", (element) =>
+    removeClass(element, "hidden")
+  );
+}
+
+/**
+ * Testa se um CPF é valido e seta a validity do ev.target de acordo
+ * @param {Event} ev
+ */
+function onCPFChange(ev) {
+  const { target } = ev;
+  testEmpty(target);
+  if (testaCPF(target.value)) target.setCustomValidity("");
+  else target.setCustomValidity("CPF invalido");
+}
+
+/**
+ * Verifica um CPF
+ * @param {string} cpf O CPF a ser testado
+ * @returns {boolean} true se o cpf for valido e false se não for
+ */
+function testaCPF(cpf) {
+  if (cpf.length < 11) return false;
+  return testDigito(cpf, 9) && testDigito(cpf, 10);
+}
+
+/**
+ * Verifica um digito do CPF
+ * @param {string} cpf O CPF a ser testado
+ * @param {number} pos A posição do digito verificador
+ * @returns {boolean} true se o digito for valido e false se não for
+ */
+function testDigito(cpf, pos) {
+  if (cpf < pos) return false;
+  let soma = 0;
+  for (let i = 0; i < pos; i++) {
+    let char = cpf[i];
+    let num = parseInt(char);
+    if (isNaN(num)) return false;
+    soma += num * (pos + 1 - i);
+  }
+  if ((soma * 10) % 11 != cpf[pos]) return false;
+  return true;
+}
